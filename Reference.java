f2e79158264a8678da88a123e6f4bbee13543317
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2024-12-29 16:06:46 +0500

    Reference Type

---
+++ Reference.java
@@ -0,0 +1,12 @@
+
+import java.util.Date;
+
+public class Reference {
+    public static void main(String[] args) {
+        String content = "            Hello, I am learning Java for DSA     ";
+        Date currentTime = new Date();
+        System.out.println(currentTime.getTime());
+        System.out.println(content.trim());
+        System.out.println(content);
+    }
+}
