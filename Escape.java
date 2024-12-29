9961c6df651cfb7e5a72353bb1770f17683b4ed2
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2024-12-29 18:40:53 +0500

    escape sequence

---
+++ Escape.java
@@ -0,0 +1,6 @@
+public class Escape {
+    public static void main(String[] args) {
+        String escapeSequence = "C:\\windows\t\\java\nescapesequnce\"Hi\"";
+        System.out.println(escapeSequence);
+    }
+}
