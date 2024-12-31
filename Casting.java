db2f59e188c0d644482e95808c4735f1b56b8804
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2024-12-31 21:10:52 +0500

    Type Casting

---
+++ Casting.java
@@ -0,0 +1,13 @@
+public class Casting {
+
+    public static void main(String[] args) {
+        // implicent type casting
+        short input1 = 30;
+        int outPut1 = input1 + 7;
+        System.out.println(outPut1);
+        // Explicit type casting
+        String firstInput = "3.4";
+        float outPut = Float.parseFloat(firstInput) + 5.3F;
+        System.out.println(outPut);
+    }
+}
